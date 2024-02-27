4d88ce71a138bf765032540f0b7801aedda22946
10922337 <10922337@o365st.cycu.edu.tw>
2024-02-27 20:18:42 +0800

    02/27上課成品

--- 20240227/10922337.py
+++ 20240227/10922337.py
@@ -1,1 +1,23 @@
 print('Hello, World!')
+
+#https://news.pts.org.tw/xml/newsfeed.xml
+import requests
+import feedparser
+
+url = "https://news.pts.org.tw/xml/newsfeed.xml"
+
+response = requests.get(url)
+
+feed = feedparser.parse(response.content)
+
+for entry in feed.entries:
+    print(entry.title)
+    print(entry.summary)
+
+    if '日' in entry.summary:
+        with open('C:/Users/User/Desktop/news.csv', 'a', encoding='utf-8') as file:
+            file.write(entry.title + '\n')
+            file.write(entry.summary + '\n')
+            file.write('=================================')
+
+    print("==================================")
